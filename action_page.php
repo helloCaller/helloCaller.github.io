0b324d6778ac7996118318d059e8e8e60610c17a
Luke Garwood <luke_gar@hotmail.com>
2022-01-30 15:18:53 -0500

    new php file

--- action_page.php
+++ action_page.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>The form method="post" attribute</h1>
-
-
-<p><?php echo $_POST['fname'];?></p>
-
-</body>
-</html>
+<?php
+echo 'Hello ' . htmlspecialchars($_POST["fname"]) . '!';
+?>
 
 
